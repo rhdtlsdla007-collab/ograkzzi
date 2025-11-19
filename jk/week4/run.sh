ea16073a2b63951634d8107d72db3f02f47e5213
jeongin <darktool2580@naver.com>
2025-11-19 14:41:10 +0900

    초기화

--- jk/week4/run.sh
+++
@@ -1,6 +0,0 @@
-rm -rf *.log simv* *.key csrc *.h *.fsdb
-
-vcs -full64 -sverilog -kdb -lca -debug_access+all -timescale="1ns/100ps" -l comp.log -ntb_opts uvm-ieee-2020-2.0 -l comp.log ./top.sv
- 
-./simv -l simv.log  +UVM_TESTNAME=jk_ubus_write_test  +UVM_VERBOSITY=UVM_MEDIUM
-
